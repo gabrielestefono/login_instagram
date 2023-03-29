dbdcff165dc33ac7622bcc73af45922f4c48fddd
gabrielestefono <gabrielestefono@hotmail.com>
2023-03-29 10:25:39 -0300

    Implementação da lógica no input da senha

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,31 @@
-let span1 = document.createElement('span'); //'<span class="form__area--box_legend-title">Telefone, nome de usuário ou email</span>';
-span1.className = "form__area--box_legend-title";
-span1.textContent = "Telefone, nome de usuário ou email"
+// Criando Elemento span para título do espaço de inserção do email
+let span1 = document.createElement('span');
+span1.className = 'form__area--box_legend-title';
+span1.textContent = 'Telefone, nome de usuário ou email'
 span1.id = 'span-1';
-let span2 = '<span class="form__area--box_legend-title">Senha</span>';
-let botao = '<a href="" class="form__area--box_legend-button">Mostrar</a>';
-let legenda1 = document.getElementById('legend-box-1');
-let box1 = document.getElementById('box-1');
-let email = document.getElementById('email');
 
+
+// Criando Elemento span para título do espaço de inserção da senha
+let span2 = document.createElement('span');'<span class="form__area--box_legend-title">Senha</span>';
+span2.className = 'form__area--box_legend-title';
+span2.textContent = 'Senha';
+span2.id = 'span2'
+
+
+// Criando Elemento botão para visualizar a senha, caso necessite
+let botao = '<a href="" class="form__area--box_legend-button">Mostrar</a>';
+
+
+// Criando todas as váriáveis necessárias através do DOM
+let legenda1 = document.getElementById('legend-box-1');
+let legenda2 = document.getElementById('legend-box-2');
+let box1 = document.getElementById('box-1');
+let box2 = document.getElementById('box-2');
+let email = document.getElementById('email');
+let pass = document.getElementById('pass');
+
+
+// Criando todas as funções necessárias para alternar entre placeholder e título com uma certa fluidez
 let mudaCaixaEmail = ()=>{
     if(email.value == ''){
         box1.className = 'form__area--box-1';
@@ -16,10 +34,25 @@ let mudaCaixaEmail = ()=>{
     }else{
         box1.className = 'form__area--box';
         legenda1.insertBefore(span1, email);
-        let recuperaSpan = document.getElementById('span-1')
         email.className = 'form__area--box_legend-input-1';
     }
 }
 
 
+let mudaCaixaSenha = ()=>{
+    if(pass.value == ''){
+        box2.className = 'form__area--box-1';
+        pass.className = 'form__area--box_legend-input-4';
+        legenda2.removeChild(span2)
+    }else{
+        box2.className = 'form__area--box';
+        legenda2.insertBefore(span2, pass);
+        pass.className = 'form__area--box_legend-input-2';
+    }
+}
+
+// Criando função para botão de visualização da senha, caso o usuário queira
+
+// Chamada de eventos
 email.addEventListener('input', mudaCaixaEmail);
+pass.addEventListener('input', mudaCaixaSenha);
